9d2d5bc589a22148f6528948394ff3a831e61d50
martin.balch <martybalch21@live.com>
2016-09-26 15:45:35 -0700

    i tryed

--- MathLib/Test.h
+++ MathLib/Test.h
@@ -1,3 +1,24 @@
 #pragma once
 
 int doNothing(int x);
+
+int doSomething(int x);
+
+struct quad_results
+{
+	float left_root, right_root;
+	int roots; //0, 1, 2
+};
+
+quad_results quad(float a, float b, float c);
+
+float blend(float s, float t, float e);
+
+struct Point { float x, y; };
+float distance(Point P1, Point P2);
+float distance(float x1, float y1, float x2, float y2);
+
+struct Point3D { float x, y, z; };
+float inner(Point3D P1, Point3D P2);
+float inner(float x1, float y1, float z1, float x2, float y2, float z2);
+
--- MathTests/Main.cpp
+++ MathTests/Main.cpp
@@ -5,7 +5,26 @@
 
 int main()
 {
-	printf("Hello fucker %d\n", doNothing(40));
+	//printf("Hello fucker %d\n", doNothing(40));
+
+	assert(doNothing(0) == 5);
+
+	assert(doSomething(0) == -7);
+
+	assert(doSomething(1) == -4);
+
+	assert(doSomething(-1) == -8);
+
+	assert(quad(1, 4, 4).left_root == -2);
+	assert(quad(1, 4, 4).right_root == -2);
+
+	assert(quad(4, 1, -5).roots == 2);
+
+	assert(blend(0, 5, 10) == 50);
+
+	assert(distance(0, 10) == 50);
+
+
 
 	system("pause");
 
