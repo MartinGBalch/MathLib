c1b666787fdc799282527306de08ea32ae00f0f6
martin.balch <martybalch21@live.com>
2016-09-26 11:00:06 -0700

    Started a math library

---
+++ MathLib/Test.cpp
@@ -0,0 +1,6 @@
+#include "Test.h"
+
+int doNothing(int x)
+{
+	return x*x + 2 * x + 5;
+}
---
+++ MathTests/Main.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include "Test.h"
+
+int main()
+{
+	printf("Hello fucker %d\n", doNothing(40));
+
+	system("pause");
+
+}
